1c1789db8bd0e95843da604c4d395cf1528b9ef4
OleksandrGudenko <oleksandr.gudenko@gmail.com>
2019-01-16 18:52:20 +0200

    Made changes to RestAPIs. Retested for functionality.

--- homerowk_1/index.js
+++ homerowk_1/index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser')
 var studentsComponent = require('./components/students')
 var coursesComponent = require('./components/courses')
 var gradesComponent = require('./components/grades')
+var grades_experimentalComponent = require('./components/grades_experimental')
 
 app.use(bodyParser.json());
 
@@ -18,5 +19,6 @@ app.get('/', (req, res) => res.send('Welcome! Use tool like Postman to get all A
 app.use('/students', studentsComponent)
 app.use('/courses', coursesComponent)
 app.use('/grades', gradesComponent)
+app.use('/grades_experimental', grades_experimentalComponent)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
